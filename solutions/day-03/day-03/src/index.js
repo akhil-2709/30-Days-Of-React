1f372e159bf3d195b887e0a98c3cdcd4d04f6b5b
akhil-2709 <aanthireddy@umass.edu>
2023-09-23 19:10:12 -0400

    day-03 partial sol

--- solutions/day-03/day-03/src/index.js
+++ solutions/day-03/day-03/src/index.js
@@ -1,79 +1,86 @@
 // index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
 // To get the root element from the HTML document
 import asabenehImage from './images/dp.jpeg'
+import techimage from './images/frontend_technologies.png'
 // JSX element, header
-const welcome = 'Welcome to 30 Days Of React'
-const title = 'Getting Started React'
-const subtitle = 'JavaScript Library'
-const author = {
-  firstName: 'Asabeneh',
-  lastName: 'Yetayeh',
-}
-const date = 'Oct 2, 2020'
+// const welcome = 'Welcome to 30 Days Of React'
+// const title = 'Getting Started React'
+// const subtitle = 'JavaScript Library'
+// const author = {s
+//   firstName: 'Asabeneh',
+//   lastName: 'Yetayeh',
+// }
+// const date = 'Oct 2, 2020'
 
-// JSX element, header
-const header = (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
-    </div>
-  </header>
-)
+// // JSX element, header
+// const header = (
+//   <header>
+//     <div className='header-wrapper'>
+//       <h1>{welcome}</h1>
+//       <h2>{title}</h2>
+//       <h3>{subtitle}</h3>
+//       <p>
+//         Instructor: {author.firstName} {author.lastName}
+//       </p>
+//       <small>Date: {date}</small>
+//     </div>
+//   </header>
+// )
 
-const numOne = 3
-const numTwo = 2
+// const numOne = 3
+// const numTwo = 2
 
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
-)
+// const result = (
+//   <p>
+//     {numOne} + {numTwo} = {numOne + numTwo}
+//   </p>
+// )
 
-const yearBorn = 1820
-const currentYear = new Date().getFullYear()
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {' '}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-)
+// const yearBorn = 1820
+// const currentYear = new Date().getFullYear()
+// const age = currentYear - yearBorn
+// const personAge = (
+//   <p>
+//     {' '}
+//     {author.firstName} {author.lastName} is {age} years old
+//   </p>
+// )
+
+// // JSX element, main
+// const techs = ['HTML', 'CSS', 'JavaScript']
+// const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+// const user = (
+//   <div>
+//     <img src={asabenehImage} alt='asabeneh image' />
+//   </div>
+// )
 
 // JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>)
-
-const user = (
+const image_tech =  (
   <div>
-    <img src={asabenehImage} alt='asabeneh image' />
+    <img className="fit" src={techimage} alt='technologies image' />
   </div>
 )
-
-// JSX element, main
-const main = (
+const level_2_1 = (
   <main>
     <div className='main-wrapper'>
-      <p>
-        Prerequisite to get started{' '}
-        <strong>
-          <em>react.js</em>
-        </strong>
-        :
-      </p>
-      <ul>{techsFormatted}</ul>
-      {result}
-      {personAge}
-      {user}
+      {image_tech}
     </div>
   </main>
+)
+
+const level_2_2 = (
+  <div className='container'>
+    <h4 align="center">SUBSCRIBE</h4>
+    <br />
+    <p align="center">Sign up with your email address to receive news and updates</p>
+    <input align="center" type="text"></input>
+    <input align="center" type="text"></input>
+  </div>
 )
 
 const copyRight = 'Copyright 2020'
@@ -90,8 +97,8 @@ const footer = (
 // JSX element, app
 const app = (
   <div className='app'>
-    {header}
-    {main}
+    {level_2_1}
+    {level_2_2}
     {footer}
   </div>
 )
